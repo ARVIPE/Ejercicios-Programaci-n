ffb5b24f043497078ba8f2c8a47b8492daad8230
ARVIPE <arvipe@hotmail.com>
2019-10-02 22:02:35 +0200

    actualizacion
    actualizacion

---
+++ repositorio personal/src/tema2/ejercicio2bloquedeejercicios3.java
@@ -0,0 +1,53 @@
+package tema2;
+
+import javax.swing.JOptionPane;
+
+public class ejercicio2bloquedeejercicios3 {
+    public static void main(String[] args) {
+
+        String str = JOptionPane.showInputDialog("Introduzca 1 si quiere hacer raices, introduzca 2 si quiere hacer potencias, introduzca 3 si quiere calcular el resto de una división ");
+        int caso = Integer.parseInt(str);
+
+        
+		switch (caso) {
+            case 1:
+            str = JOptionPane.showInputDialog("Introduzca base de la raíz");
+            double base = Double.parseDouble(str);
+            
+
+            str = JOptionPane.showInputDialog("Introduzca un exponente");
+            double exponente = Double.parseDouble(str);
+            
+            
+            System.out.println("El resultado es:");
+            System.out.println(Math.pow(base, (1/exponente)));
+            break;
+
+            case 2:
+            str = JOptionPane.showInputDialog("Introduzca una base");
+            double base1 = Double.parseDouble(str);
+            
+
+            str = JOptionPane.showInputDialog("Introduzca un exponente");
+            double exponente1 = Double.parseDouble(str);
+            
+
+            System.out.println("El resultado es:");
+            System.out.println(Math.pow(base1, exponente1));
+            break;
+            case 3:
+            str = JOptionPane.showInputDialog("Introduzca un dividendo");
+            double dividendo = Double.parseDouble(str);
+
+            str = JOptionPane.showInputDialog("Introduzca un divisor");
+            double divisor = Double.parseDouble(str);
+            
+            System.out.println("El resultado es:");
+            System.out.println(dividendo % divisor);
+            break;
+
+
+
+        }
+    }
+}
---
+++ repositorio personal/src/tema2/ejercicio3bloquedeejercicios3.java
@@ -0,0 +1,27 @@
+package tema2;
+
+import javax.swing.JOptionPane;
+
+public class ejercicio3bloquedeejercicios3 {
+
+	public static void main(String[] args) {
+        
+        String str = JOptionPane.showInputDialog("Introduzca un número del 0 al 5 en función de lo que desee" +
+        "1. Cálculo de la hipotenusa de un triángulo." + 
+        " 2. Cálculo de la superficie de una circunferencia." +
+        " 3. Cálculo del perímetro de una circunferencia." + 
+        "4. Cálculo del área de un rectángulo." + 
+        "5. Cálculo del área de un triángulo." +
+        "0. Salir de la aplicación.");
+        
+        int num1 = Integer.parseInt(str);
+        
+
+        
+
+
+
+        
+
+    }
+}
