896afa9ccdd460f4dfc2fb65dcb3e7829e08042e
diurno <diurno@pc-aula>
2019-10-07 14:59:25 +0200

    actualización

---
+++ repositorio personal/src/tema3/ejercicio2bloque2.java
@@ -0,0 +1,23 @@
+package tema3;
+
+import javax.swing.JOptionPane;
+
+public class ejercicio2bloque2 {
+
+	public static void main(String[] args) {
+		
+		String str = JOptionPane.showInputDialog("Introduzca cuantos números quiere que se le pidan");
+		int seriedenumeros = Integer.parseInt(str);
+		
+		int i;
+		for (i = 0; i > seriedenumeros; i++) {
+			str = JOptionPane.showInputDialog("Introduzca los números");
+			int numeros = Integer.parseInt(str);
+			
+			
+			
+		}
+
+	}
+
+}
