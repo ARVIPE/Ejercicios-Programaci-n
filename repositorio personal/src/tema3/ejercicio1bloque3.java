3c396f2e0a52c3495d987f06f9448db6baa88e3f
ARVIPE <arvipe@hotmail.com>
2019-10-07 20:09:33 +0200

    actualización
    actualización

---
+++ repositorio personal/src/tema3/ejercicio1bloque3.java
@@ -0,0 +1,22 @@
+package tema3;
+
+import javax.swing.JOptionPane;
+
+public class ejercicio1bloque3 {
+
+	public static void main(String[] args) {
+		
+			int i;
+            int suma = 0;
+            int numeros;
+		
+			while numeros > 0:
+				String str = JOptionPane.showInputDialog("Introduzca una serie de números");
+                numeros = Integer.parseInt(str);
+                
+                suma = suma + numeros;
+    }
+}
+			
+		
+               
--- repositorio personal/src/tema3/ejercicio2bloque2.java
+++ repositorio personal/src/tema3/ejercicio2bloque2.java
@@ -6,21 +6,28 @@ public class ejercicio2bloque2 {
 
 	public static void main(String[] args) {
 		
-		String str = JOptionPane.showInputDialog("Introduzca cuantos números quiere que se le pidan");
-		int seriedenumeros = Integer.parseInt(str);
+		String str = JOptionPane.showInputDialog("Illo cuantos números quieres meter compadre");
+		int numero = Integer.parseInt(str);
+
 		
-		int i;
-		for (i = 0; i > seriedenumeros; i++) {
-			str = JOptionPane.showInputDialog("Introduzca los números");
-			int numeros = Integer.parseInt(str);
+
+		for(int i=0; i < numero; i++) {
+
+			String srt = JOptionPane.showInputDialog("Introduce la sucesión de números");
+			int sucesiondenumeros = Integer.parseInt(str);
+
+			if (sucesiondenumeros > sucesiondenumeros) {
+				System.out.println(sucesiondenumeros);
+
+			}else{
+				System.out.println(sucesiondenumeros);
+			}
 
 
-			
-			
-			
-			
+		
+			System.out.println("Illo tu número menor es:" + sucesiondenumeros);
+		
 		}
-
+		
 	}
-
 }
