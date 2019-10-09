263e57722e7490444526be75cf54b8462c918333
ARVIPE <arvipe@hotmail.com>
2019-10-10 00:31:44 +0200

    actualizacion
    actualizacion

---
+++ repositorio personal/src/tema3/juegoadivinarnumeroalazar.java
@@ -0,0 +1,29 @@
+package tema3;
+import javax.swing.JOptionPane;
+
+public class juegoadivinarnumeroalazar {
+ 
+	public static void main(String[] args) {
+		
+		int aleatorio=(int)(Math.random()*100);
+ 
+		int numero=0;   
+    
+    for ( ;numero != aleatorio;){
+		
+		numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese Número"));
+ 
+		if(aleatorio<numero){
+            JOptionPane.showMessageDialog(null, "Ingrese Número mas bajo");
+
+        }else{
+                    if(aleatorio>numero){
+                    JOptionPane.showMessageDialog(null, "Ingrese Número mas alto");
+
+        
+                    }
+                }
+            }
+        System.out.println("Enhorabuena has acertado el número");
+	}
+}
