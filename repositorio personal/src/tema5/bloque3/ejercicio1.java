3f5ebb471af2c84844d1198510f2eac5abcfa175
ARVIPE <arvipe@hotmail.com>
2019-10-24 23:23:57 +0200

    actualización
    actualización

--- repositorio personal/src/tema5/bloque3/ejercicio1.java
+++ repositorio personal/src/tema5/bloque3/ejercicio1.java
@@ -4,31 +4,40 @@ package tema5.bloque3;
 public class ejercicio1 {
 
 	public static void main(String[] args) {
-		// Declaraci�n del array
-		int array[] = new int[20];
+
+		int array[] = new int [20];
+		
+		
 		
 		for (int i = 0; i < array.length; i++) {
-
-			int candidato; 
-			boolean existeCandidatoEnArray;  
-		
-			do {
-				candidato = Utils.obtenerNumeroAzar1(20);
-				existeCandidatoEnArray = false;
-				for (int j = 0; j < i; j++) {
-					if (array[j] == candidato) {
-						existeCandidatoEnArray = true;
-					}
-				}
-			} while (existeCandidatoEnArray);
-	
-			array[i] = candidato;
+			array[i] = (int) Math.round(Math.random() * (20));    
 		}
 		
-	
-		System.out.println(array);
+		// Imprimir en la consola el array creado
+		for (int i = 0; i < array.length; i++) {
+			
+		
+
+			System.out.print(array[i] + "    ");
+		}
+
+		for (int i = 0; i < array.length - 1 ; i++){
+
+			for (int j = 0; j < array. length - 1; j++){
+				if (array[j] > array[j+1]){
+					
+					int aux = array[j];
+					array[j] = array[j+1];
+					array[j+1] = aux;
+					
+				
+				}
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + "    ");
+		}
+
 	}
-
 }
-
-	
---
+++ repositorio personal/src/tema5/bloque3/ejercicio2.java
@@ -0,0 +1,10 @@
+package tema5.bloque3;
+
+public class ejercicio2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
