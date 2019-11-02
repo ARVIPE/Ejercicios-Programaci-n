16401b504eb698c2160fe28ef1fd29b83e25cb25
ARVIPE <arvipe@hotmail.com>
2019-11-02 23:56:44 +0100

    actualizacion
    actualizacion

--- repositorio personal/src/tema5/bloque4/ejercicio4.java
+++ repositorio personal/src/tema5/bloque4/ejercicio4.java
@@ -5,13 +5,30 @@ public class ejercicio4 {
 	public static void main(String[] args) {
         
         int matriz[][] = new int[5][5];
-
-        for (int x=0; x < matriz.length; x++) {
-            for (int y=0; y < matriz[x].length; y++) {
-              matriz[x][y] = (int) (Math.random()*9+1);
+        int x = 0;
+        int y = 0;
+        
+        for (x=0; x < matriz.length; x++) {
+            for (y=0; y < matriz[x].length; y++) {
+              matriz[x][y] = (int) (Math.random()* (10 - -10) + -10);
             }
           }
-
+        
+        for(x = 0; x < matriz.length; x++) {
+        	for(y = 0; y < matriz.length; y++) {
+        
+        	System.out.print(matriz[x][y] + "\t");
+        	}
+        	System.out.println();
+        }
+        
+        if(matriz[x][y] >= 0) {
+        	System.out.println("La matríz es positiva");
+        }else {
+        	System.out.println("La matríz no es positiva");
+        }
+        
+        
 
     }
 }
