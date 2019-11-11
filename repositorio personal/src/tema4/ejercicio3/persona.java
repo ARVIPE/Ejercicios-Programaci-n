783b71ba29a9f53798d99136c82671cb8b794d9c
ARVIPE <arvipe@hotmail.com>
2019-11-11 15:01:27 +0100

    actualización

---
+++ repositorio personal/src/tema4/ejercicio3/persona.java
@@ -0,0 +1,23 @@
+package tema4.ejercicio3;
+
+public class persona {
+
+    String nombre;
+    String apellidos;
+    public int DNI;
+    public int direccion;
+    public int telefono;
+    
+
+   /**
+    * 
+    */
+    public void imprimeEnConsola () {
+        System.out.println("Persona: nombre" + nombre + " - apellidos: " +
+        apellidos + " - DNI:" + DNI + )
+
+    
+
+
+
+}
