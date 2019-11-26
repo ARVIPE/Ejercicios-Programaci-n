0e94fad0229773ed3ea3c00fe1985a480b8984f5
ARVIPE <arvipe@hotmail.com>
2019-11-26 13:02:04 +0100

    actualizacion

---
+++ repositorio personal/src/tema4/ejercicio_TiendaAntigüedades/ColeccionAntiguedades.java
@@ -0,0 +1,18 @@
+package tema4.ejercicio_TiendaAntigüedades;
+
+public class ColeccionAntiguedades {
+
+	public static void main(String[] args) {
+		
+		Antiguedad joya;
+		private Antiguedad libros;
+		
+		public joya(int material) {
+			this.material = material;
+		}
+		
+		
+
+	}
+
+}
---
+++ repositorio personal/src/tema4/ejercicio_TiendaAntigüedades/Principal.java
@@ -0,0 +1,19 @@
+package tema4.ejercicio_TiendaAntigüedades;
+
+
+
+public class Principal {
+
+
+	public static void main(String[] args) {
+		
+		Antiguedad tienda = new Antiguedad(1942, "españa", 13); //Año, origen, precio
+		
+		tienda.imprimir();
+		
+		
+		
+
+	}
+
+}
