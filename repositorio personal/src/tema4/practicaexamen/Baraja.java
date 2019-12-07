16a856f54e869b8faa6d89fed1279e8db69bf9ef
ARVIPE <arvipe@hotmail.com>
2019-12-07 17:29:42 +0100

    actualización

--- repositorio personal/src/tema4/practicaexamen/Baraja.java
+++ repositorio personal/src/tema4/practicaexamen/Baraja.java
@@ -3,55 +3,58 @@ package tema4.practicaexamen;
 import java.util.Arrays;
 
 public class Baraja {
-	Carta carta[] = new Carta[51];
-	
-	private String picas;
-	private String diamantes;
-	private String treboles;
-	private String corazones;
-	
-	
-	public void Baraja() {
-		
-		for(int i = 0; i < 13; i++) {
-		
-			
-			carta[i] = new Carta(i+1, "picas", i + "picas", i );
+	Carta carta[] = new Carta[52];
+
+	public Baraja() {
+		int j = 0;
+		for (int i = 0; i < 13; i++) {
+			carta[j] = new Carta(i + 1, "Picas", j);
+			j++;
 		}
-		
-		for(int i = 0; i < 27; i++) {
-		
-			
-			carta[i] = new Carta(i+1, "diamantes", i + "diamantes", i );
+		for (int i = 0; i < 13; i++) {
+			carta[j] = new Carta(i + 1, "Diamantes", j);
+			j++;
 		}
-		
-		for(int i = 0; i < 40; i++) {
-			
-			
-			carta[i] = new Carta(i+1, "treboles", i + "treboles", i );
+		for (int i = 0; i < 13; i++) {
+			carta[j] = new Carta(i + 1, "Treboles", j);
+			j++;
 		}
-		
-		for(int i = 0; i < carta.length; i++) {
-		
-			
-			carta[i] = new Carta(i+1, "corazones", i + "corazones", i );
+		for (int i = 0; i < 13; i++) {
+			carta[j] = new Carta(i + 1, "Corazones", j);
+			j++;
 		}
-		
-		
-	
-		
-		
-		
+
 	}
 
-	@Override
-	public String toString() {
-		return "Baraja [carta=" + Arrays.toString(carta) + ", picas=" + picas + ", diamantes=" + diamantes
-				+ ", treboles=" + treboles + ", corazones=" + corazones + "]";
+	/*
+	 * 
+	 */
+
+	public void cartasdeArribaHaciaAbajo() {
+		for (int i = 1; i < carta.length; i++) {
+			Carta aux = carta[i - 1];
+			carta[i - 1] = carta[i];
+			carta[i] = aux;
+		}
 	}
-	
 
-	
-	
+	public void cartasdeAbajoHaciaArriba() {
+		for (int i = 1; i < carta.length; i--) {
+			Carta aux = carta[i];
+			carta[i] = carta[i - 1];
+			carta[i - 1] = aux;
+		}
 
-}
+	}
+
+	public void imprimir() {
+		System.out.println("Baraja");
+		for (int i = 0; i < carta.length; i++) {
+			System.out.println(carta[i]);
+		}
+	}
+}		
+				
+				
+				
+				
--- repositorio personal/src/tema4/practicaexamen/Principal.java
+++ repositorio personal/src/tema4/practicaexamen/Principal.java
@@ -3,11 +3,19 @@ package tema4.practicaexamen;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		Baraja baraja = new Baraja();
-		
-		System.out.println(baraja.toString());
-		
+
+		baraja.imprimir();
+
+		System.out.println("Carta de arriba hacia abajo es: ");
+		baraja.cartasdeArribaHaciaAbajo();
+		baraja.imprimir();
+
+		System.out.println("Carta de abajo hacia arriba es: ");
+		baraja.cartasdeAbajoHaciaArriba();
+		baraja.imprimir();
+
 	}
 
 }
