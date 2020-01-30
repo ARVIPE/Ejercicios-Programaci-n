ef117909a722e6411b7fb8c52e8714989908acfa
ARVIPE <arvipe@hotmail.com>
2020-01-30 18:13:19 +0100

    Revert "actualizacion"
    This reverts commit 01b9ec2d260870d6083bb4e6246d04cb6c34b803.

--- repositorio personal/src/Juegos/Arkanoid/Codigo/Pelota.java
+++ repositorio personal/src/Juegos/Arkanoid/Codigo/Pelota.java
@@ -1,6 +1,6 @@
 package Juegos.Arkanoid.Codigo;
 
-import java.awt.Color;
+import java.awt.Color;	
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -8,19 +8,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class Pelota extends Objeto implements KeyListener, MouseListener {
+public class Pelota extends Objeto implements KeyListener, MouseListener{
 
 	protected int vx;
 	protected int vy;
 	private boolean space;
 	protected static final int PLAYER_SPEED = 4;
 	private int contador = 0;
-	// Variable ficticia para medir el tiempo
+	//Variable ficticia para medir el tiempo
 	private long usedTime;
-	// Variable con la que empieza a contar el tiempo en milis
-	long startTime = System.currentTimeMillis();
+	//Variable con la que empieza a contar el tiempo en milis
+	long startTime = System.currentTimeMillis(); 
 	int contadortiempo = 0;
-
+	
 	public Pelota() {
 		super();
 		this.color = Color.gray;
@@ -30,60 +30,58 @@ public class Pelota extends Objeto implements KeyListener, MouseListener {
 		this.vy = 0;
 
 	}
-
+	
+	
 	@Override
 	public void paint(Graphics g) {
 		g.setColor(getColor());
-		g.fillOval(this.xCoord, this.yCoord, this.ancho, this.alto);
+		g.fillOval(this.xCoord, this.yCoord, this.ancho, this.alto);		
 	}
-
+	
 	public void tiempo() {
-		// Unidad de tiempo que empieza en cero
+		//Unidad de tiempo que empieza en cero
 		usedTime = System.currentTimeMillis() - startTime;
-		if (usedTime >= 5000 && contadortiempo == 0 && contador == 0) {
+		if(usedTime >= 5000 && contadortiempo == 0 && contador == 0) {
 			contadortiempo++;
 			contador++;
 			vx = 3;
 			vy = 3;
-
+		
 		}
 	}
-
+	
 	public void act() {
 		tiempo();
-		// Si la velocidad de la pelota es 0 aparece en las mismas coordenadas que la
-		// nave
-		if (this.vx == 0 && this.vy == 0) {
+		//Si la velocidad de la pelota es 0 aparece en las mismas coordenadas que la nave
+		if(this.vx == 0 && this.vy == 0) {
 			this.xCoord = Arkanoid.getInstace().getNave().getxCoord() + 17;
 			this.yCoord = Arkanoid.getInstace().getNave().getyCoord() - 15;
 		}
-
-		// Vamos aumentando la velocidad horizontal y verticalmente
+		
+		//Vamos aumentando la velocidad horizontal y verticalmente
 		this.xCoord += this.vx;
 		this.yCoord += this.vy;
-
-		// Le indicamos que si la pelota llega al borde izquierdo o derecho cambie de
-		// sentido
-		// Tenemos que tener en cuenta que el borde izquierdo es 0 pero el derecho es la
-		// extension de la ventana
-		// menos la de la pelota
-		if (this.xCoord < 0 || this.xCoord > (Arkanoid.getInstace().getWidth() - 15)) {
+		
+		//Le indicamos que si la pelota llega al borde izquierdo o derecho cambie de sentido
+		//Tenemos que tener en cuenta que el borde izquierdo es 0 pero el derecho es la extension de la ventana
+		//menos la de la pelota
+		if(this.xCoord < 0 || this.xCoord > (Arkanoid.getInstace().getWidth() - 15)) {
 			vx = -vx;
-
+		
 		}
-		// Le indicamos que si la pelota llega al borde superior o inferior cambie de
-		// sentido
-		if (this.yCoord < 0 || this.yCoord > (Arkanoid.getInstace().getHeight() - 60)) {
+		//Le indicamos que si la pelota llega al borde superior o inferior cambie de sentido
+		if(this.yCoord < 0 || this.yCoord > (Arkanoid.getInstace().getHeight() - 60)) {
 			vy = -vy;
 		}
-
+		
+		
 	}
-
+	
 	@Override
 	public void collisionWith(Objeto actorCollisioned) {
 		super.collisionWith(actorCollisioned);
 
-		if (actorCollisioned instanceof Ladrillo) {
+		if (actorCollisioned instanceof Ladrillo){
 			this.vy = -this.vy;
 			SoundRepository.getInstance().playSound(SoundRepository.EXPLOSION);
 		}
@@ -93,73 +91,81 @@ public class Pelota extends Objeto implements KeyListener, MouseListener {
 		}
 	}
 
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (contador == 0) {
+		if(contador == 0) {
 			vx = 3;
 			vy = 3;
 		}
 		contador++;
 	}
 
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
+			 
+
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_SPACE:
-			space = true;
-			break;
-		}
-		updateSpeed();
-		contador++;
+	  	switch (e.getKeyCode()) {
+			case KeyEvent.VK_SPACE : space = true; break;
+	  	}
+	  	updateSpeed();
+	  	contador++;
 	}
+
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
-		case KeyEvent.VK_SPACE:
-			space = false;
-			break;
+			case KeyEvent.VK_SPACE : space = false; break; 
 		}
 		updateSpeed();
 	}
-
+	
 	protected void updateSpeed() {
-		if (contador == 0) {
-			if (space)
-				this.vx = 3;
-			if (space)
-				this.vy = 3;
+		if(contador == 0) {
+			if (space) this.vx = 3;
+			if (space) this.vy = 3;
 		}
 	}
+	
+	
+
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
+	
+
+
 
 }
